405df4a63ed1140894bc703225b2dfe17fe142af
Jessica Batista <jessica21943@hotmail.com>
2018-09-20 20:36:24 -0300

    distancia entre dois pontos

---
+++ ex02/main.py
@@ -0,0 +1,12 @@
+import math
+
+x1 = int(input("Coordenada x1 de P1:"))
+y1 = int(input("Coordenada y1 de P1: "))
+x2 = int(input("Coordenada x2 de P2: "))
+y2 = int(input("Coordenada y2 de P2: "))
+d = math.sqrt( (x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+print("distancia entre P1 e P2 e:", d)
+
+
+
