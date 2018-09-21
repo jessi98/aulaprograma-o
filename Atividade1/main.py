d29ec251f05bd54a8a6df05b76c71ce0495cc402
Jessica Batista <jessica21943@hotmail.com>
2018-09-20 21:58:47 -0300

    Atividade

---
+++ Atividade1/main.py
@@ -0,0 +1,22 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+c = int(input("Digite o terceiro numero: "))
+
+
+if b>a>c:
+    print ("O maior numero e", b, "O menor numero e", c )
+
+if a>c>b:
+    print ("O maior numero e", a, "O menor numero e", b )
+
+if c>b>a:
+    print ("O maior numero e", c, "O menor numero e", a )
+
+
+
+
+
+
+
+
+
